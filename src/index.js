400f32dfa465fe5a39267f39413e2e353cabb2bb
iamtashi <tacmoktan@gmail.com>
2018-08-23 00:19:47 +0545

    little content added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
-console.log('this is master');
-console.log('just some extra text');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Master extends React.Component{
+   
+    render() {
+        console.log('this is master');
+        console.log('just some extra text');
+        return (
+        <div><h2>Welcome to Master branch</h2>
+        <h4>Checkout other branches</h4>
+        </div>
+        );
+    }
+}
+ReactDOM.render(<Master/>,document.getElementById('root'));
 /*
 import registerServiceWorker from './registerServiceWorker';
 
