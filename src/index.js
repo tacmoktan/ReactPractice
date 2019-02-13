cb8ba95c3054fc0bee4e224534a218e3927c01fb
iamtashi <tacmoktan@gmail.com>
2019-02-13 20:03:48 +0545

    added mounting life cycle

--- src/index.js
+++ src/index.js
@@ -1,20 +1,31 @@
+//# Mounting lifecycle
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Master extends React.Component{
-   
+class ComponentClass extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {value:""};
+    }
+    
+    componentWillMount(){  //called before render()
+        alert('time to change state');
+        this.setState({value:"will mount"});
+    }
+
+    componentDidMount(){ //called after render()
+        alert('already changed state');
+        this.setState({value:"did mount"});
+    }
+
     render() {
-        console.log('this is master');
-        console.log('just some extra text');
-        return (
-        <div><h2>Welcome to Master branch</h2>
-        <h4>Checkout other branches</h4>
-        </div>
-        );
+        return <h1>{this.state.value}</h1>;
     }
 }
-ReactDOM.render(<Master/>,document.getElementById('root'));
+
+ReactDOM.render(<ComponentClass />, document.querySelector('#root'));
+
 /*
 import registerServiceWorker from './registerServiceWorker';
 
